b577efd40a9d29be2dcea7a20e285ace24ecee14
Claudia Falk <claudiafalk0@gmail.com>
2019-09-09 20:15:40 -0600

    add logs

--- sign_s3.js
+++ sign_s3.js
@@ -4,33 +4,21 @@ require('dotenv').config(); // Configure dotenv to load in the .env file
 const pupperUrl = 'guarded-escarpment-41457.herokuapp.com';
 const S3_BUCKET = process.env.Bucket;
 const BC_S3_BUCKET = process.env.bc_Bucket;
+let s3Params = {};
+
 // Now lets export this function so we can call it from somewhere else
 exports.sign_s3 = (req, res) => {
+    const s3 = new aws.S3();  // Create a new instance of S3gi
+    const fileName = req.body.fileName;
+    const fileType = req.body.fileType;
     const ref = new URL(req.get('origin'));
-    console.log('REFURL:', ref);
+
     if (ref.hostname === pupperUrl) {
         aws.config.update({
             region: 'us-east-2', // Put your aws region here
             accessKeyId: process.env.AWSAccessKeyId,
             secretAccessKey: process.env.AWSSecretKey
-        })
-    } else {
-        aws.config.update({
-            region: 'us-east-2', // Put your aws region here
-            accessKeyId: process.env.bc_AWSAccessKeyId,
-            secretAccessKey: process.env.bc_AWSSecretKey
-        })
-    }
-
-
-    console.log("INSIDE SIGN_S3 FUNC \n\n fileName: " + req.body.fileName + "\nfileType: " + req.body.fileType);
-    const s3 = new aws.S3();  // Create a new instance of S3gi
-    const fileName = req.body.fileName;
-    const fileType = req.body.fileType;
-// Set up the payload of what we are sending to the S3 api
-    let s3Params = {};
-
-    if(req.header.referer === pupperUrl){
+        });
         s3Params = {
             Bucket: S3_BUCKET,
             Key: fileName,
@@ -38,14 +26,19 @@ exports.sign_s3 = (req, res) => {
             ContentType: fileType,
             ACL: 'public-read'
         }
-    }else{
+    } else {
+        aws.config.update({
+            region: 'us-east-2', // Put your aws region here
+            accessKeyId: process.env.bc_AWSAccessKeyId,
+            secretAccessKey: process.env.bc_AWSSecretKey
+        });
         s3Params = {
             Bucket: BC_S3_BUCKET,
             Key: fileName,
             Expires: 50,
             ContentType: fileType,
             ACL: 'public-read'
-    }
+        }
     }
 
 // Make a request to the S3 API to get a signed URL which we can use to upload our file
