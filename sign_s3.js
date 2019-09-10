ff9c7d66d6b216207705e84131ef5247f44c1173
Claudia Falk <claudiafalk0@gmail.com>
2019-09-09 19:52:31 -0600

    if else

--- sign_s3.js
+++ sign_s3.js
@@ -1,28 +1,51 @@
 const aws = require('aws-sdk');
 require('dotenv').config(); // Configure dotenv to load in the .env file
 // Configure aws with your accessKeyId and your secretAccessKey
+const pupperUrl = 'https://guarded-escarpment-41457.herokuapp.com/';
 const S3_BUCKET = process.env.Bucket;
-const BC_S3_BUCKET=process.env.bc_Bucket;
+const BC_S3_BUCKET = process.env.bc_Bucket;
 // Now lets export this function so we can call it from somewhere else
 exports.sign_s3 = (req, res) => {
-    aws.config.update({
-        region: 'us-east-2', // Put your aws region here
-        accessKeyId: req.headers.referer = 'https://guarded-escarpment-41457.herokuapp.com/' ? process.env.AWSAccessKeyId : process.env.bc_AWSAccessKeyId,
-        secretAccessKey: req.headers.referer = 'https://guarded-escarpment-41457.herokuapp.com/' ? process.env.AWSSecretKey : process.env.bc_AWSSecretKey
-    });
+    if (req.header.referer === pupperUrl) {
+        aws.config.update({
+            region: 'us-east-2', // Put your aws region here
+            accessKeyId: process.env.AWSAccessKeyId,
+            secretAccessKey: process.env.AWSSecretKey
+        })
+    } else {
+        aws.config.update({
+            region: 'us-east-2', // Put your aws region here
+            accessKeyId: process.env.bc_AWSAccessKeyId,
+            secretAccessKey: process.env.bc_AWSSecretKey
+        })
+    }
+
 
     console.log("INSIDE SIGN_S3 FUNC \n\n fileName: " + req.body.fileName + "\nfileType: " + req.body.fileType);
     const s3 = new aws.S3();  // Create a new instance of S3gi
     const fileName = req.body.fileName;
     const fileType = req.body.fileType;
 // Set up the payload of what we are sending to the S3 api
-    const s3Params = {
-        Bucket: req.headers.referer = 'https://guarded-escarpment-41457.herokuapp.com/' ? S3_BUCKET : BC_S3_BUCKET,
-        Key: fileName,
-        Expires: 50,
-        ContentType: fileType,
-        ACL: 'public-read'
-    };
+    let s3Params = {};
+
+    if(req.header.referer === pupperUrl){
+        s3Params = {
+            Bucket: S3_BUCKET,
+            Key: fileName,
+            Expires: 50,
+            ContentType: fileType,
+            ACL: 'public-read'
+        }
+    }else{
+        s3Params = {
+            Bucket: BC_S3_BUCKET,
+            Key: fileName,
+            Expires: 50,
+            ContentType: fileType,
+            ACL: 'public-read'
+    }
+    }
+
 // Make a request to the S3 API to get a signed URL which we can use to upload our file
     s3.getSignedUrl('putObject', s3Params, (err, data) => {
         if (err) {
